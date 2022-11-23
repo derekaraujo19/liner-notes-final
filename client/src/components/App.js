31f97137209f3359f892aade3afbe978a3be5e41
Derek Araujo <derekaraujo19@gmail.com>
2022-11-23 18:23:06 -0500

    Sign Up feature implemented

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Auth from "./Auth";
 
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../App.css';
 
 function App() {
   const [user, setUser] = useState(null)
 
+  console.log(user)
+
+  // Stay Logged In:
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  // Log Out (needs to be moved!)
+  function handleLogout() {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    })
+  }
+
+
   if(!user) return (
-    <Auth />
+    <Auth user={user} setUser={setUser}/>
   )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleLogout()}> Log Out </button>
     </div>
   );
 }
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,6 +1,54 @@
-function Auth() {
+import React, {useState} from "react";
+
+function Auth({user, setUser}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  // console.log(passwordConfirm)
+
+  // Sign Up
+  function handleSignupSubmit(e){
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "username":newUsername,
+        "password":newPassword,
+        "password_confirmation":passwordConfirm,
+      }),
+    }).then((r)=> {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      } else {
+        r.json().then((err) => console.log(err.errors))
+      }
+    });
+  }
+
   return (
-    <h1>Sign up or Sign in! </h1>
+    <div>
+      {/* Log In Form */}
+      <h1> Log In </h1>
+      <form>
+        <input type="text" name="username" placeholder="Username" autoComplete="off" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input type="text" name="password" placeholder="Password" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button> Submit </button>
+      </form>
+      {/* Sign Up Form */}
+      <h1> Or Create an Account </h1>
+      <form onSubmit={handleSignupSubmit}>
+        <input type="text" name="username" placeholder="Username" autoComplete="off" value={newUsername} onChange={(e) => setNewUsername(e.target.value)} />
+        <input type="text" name="password" placeholder="Password" autoComplete="off" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+        <input type="text" name="password_confirmation" placeholder="Confirm Password" autoComplete="off" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+        <button> Create Account </button>
+      </form>
+    </div>
   )
 }
 
